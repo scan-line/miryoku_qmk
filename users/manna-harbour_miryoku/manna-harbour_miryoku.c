a709075763b17a42fefb1cf0f61d8626de8e8cf3
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-05 00:47:54 +0000

    Update manna-harbour_miryoku.c

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -70,8 +70,6 @@ const uint16_t PROGMEM os_keys[][OS_CLIP_END] = {
 // TODO - save/restore eeprom
 static os_mode_t os_mode = OS_DEFAULT_MODE;
 
-extern keymap_config_t keymap_config;
-
 #endif
 
 
@@ -152,11 +150,9 @@ bool process_os_mode_key(os_mode_t mode, keyrecord_t *record) {
   if (record->event.pressed) {
     os_mode = mode;
     if (mode == OS_MODE_MAC) {
-        keymap_config.swap_lctl_lgui = true;
-        keymap_config.swap_rctl_rgui = true;
+        process_magic(MAGIC_SWAP_CTL_GUI, record);
     } else {
-        keymap_config.swap_lctl_lgui = false;
-        keymap_config.swap_rctl_rgui = false;
+        process_magic(MAGIC_UNSWAP_CTL_GUI, record);
     }
   }
   return false;
