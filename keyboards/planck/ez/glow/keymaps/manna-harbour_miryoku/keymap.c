c072ff404d764fdaf569e464a3928d8ec5cf308f
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-11 18:06:47 +0000

    Update keymap.c

--- keyboards/planck/ez/glow/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/planck/ez/glow/keymaps/manna-harbour_miryoku/keymap.c
@@ -1,1 +1,30 @@
 // TODO - Copyright
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  planck_ez_left_led_off();
+  planck_ez_right_led_off();
+  uint8_t layer = biton32(state);
+  switch (layer) {
+    case U_BASE:
+    case U_EXTRA:
+    case U_TAP:
+      break;
+    case U_NAV:
+    case U_MOUSE:
+    case U_MEDIA:
+      planck_ez_left_led_on();
+      break;
+    case U_NUM:
+    case U_SYM:
+    case U_FUN:
+      planck_ez_right_led_on();
+      break;
+    case U_BUTTON:
+      planck_ez_left_led_on();
+      planck_ez_right_led_on();
+      break;
+    default:
+      break;
+    }
+    return state;
+}
