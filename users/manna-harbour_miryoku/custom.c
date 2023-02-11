98114347e4d768910bed9d3c7f30dac344d767a6
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-11 20:25:21 +0000

    Update custom.c

--- users/manna-harbour_miryoku/custom.c
+++ users/manna-harbour_miryoku/custom.c
@@ -153,11 +153,14 @@ __attribute__((weak)) void custom_show_layer(uint8_t layer)(layer_state_t state)
 }
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
+  uint8_t layer = get_highest_layer(state);
+  send_string(" default layer change\n");
   return state;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   uint8_t layer = get_highest_layer(state);
+  send_string(" layer change\n");
   custom_show_layer(state);
 }
 
