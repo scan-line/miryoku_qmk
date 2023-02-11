5d02441d74266f9510f40f026f43e7b5cce3114f
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-11 17:33:33 +0000

    Update custom.c

--- users/manna-harbour_miryoku/custom.c
+++ users/manna-harbour_miryoku/custom.c
@@ -153,24 +153,25 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   planck_ez_right_led_off();
   uint8_t layer = biton32(state);
   switch (layer) {
+    case U_BASE:
+    case U_EXTRA:
+    case U_TAP:
+      break;
+    case U_NAV:
+    case U_MOUSE:
+    case U_MEDIA:
+      planck_ez_left_led_on();
+      break;
+    case U_NUM:
+    case U_SYM:
+    case U_FUN:
+      planck_ez_right_led_on();
+      break;
+    case U_BUTTON:
+      planck_ez_left_led_on();
+      planck_ez_right_led_on();
+      break;
     default:
-    case BASE:
-    case EXTRA:
-    case TAP:
-      break;
-    case NAV:
-    case MOUSE:
-    case MEDIA:
-      planck_ez_left_led_on();
-      break;
-    case NUM:
-    case SYM:
-    case FUN:
-      planck_ez_right_led_on();
-      break;
-    case BUTTON:
-      planck_ez_left_led_on();
-      planck_ez_right_led_on();
       break;
     }
     return state;
