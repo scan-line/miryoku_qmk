9a601fec1304211e5fd8d18d7eda7c07183871cd
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-11 19:17:18 +0000

    Update keymap.c

--- keyboards/planck/ez/glow/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/planck/ez/glow/keymaps/manna-harbour_miryoku/keymap.c
@@ -19,40 +19,20 @@ layer_state_t layer_state_set_user2(layer_state_t state) {
   uint8_t layer = get_highest_layer(state);
   switch (layer) {
     case U_BASE:
-      send_string(" base ");
-      break;
     case U_EXTRA:
-      send_string(" extra ");
-      break;
     case U_TAP:
-      send_string(" tap ");
       break;
     case U_NAV:
-      send_string(" nav ");
-      planck_ez_left_led_on();
-      break;
     case U_MOUSE:
-      send_string(" mouse ");
-      planck_ez_left_led_on();
-      break;
     case U_MEDIA:
-      send_string(" media ");
       planck_ez_left_led_on();
       break;
     case U_NUM:
-      send_string(" num ");
-      planck_ez_right_led_on();
-      break;
     case U_SYM:
-      send_string(" sym ");
-      planck_ez_right_led_on();
-      break;
     case U_FUN:
-      send_string(" fun ");
       planck_ez_right_led_on();
       break;
     case U_BUTTON:
-      send_string(" button ");
       planck_ez_left_led_on();
       planck_ez_right_led_on();
       break;
