950871c1b98017a529f114d02f6aa8fbf5948bd0
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-11 19:55:50 +0000

    Update custom.c

--- users/manna-harbour_miryoku/custom.c
+++ users/manna-harbour_miryoku/custom.c
@@ -153,7 +153,32 @@ __attribute__((weak)) layer_state_t layer_state_set_user2(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return layer_state_set_user2(state);
+  planck_ez_left_led_off();
+  planck_ez_right_led_off();
+  uint8_t layer = get_highest_layer(state);
+  switch (layer) {
+    case U_BASE:
+    case U_EXTRA:
+    case U_TAP:
+      break;
+    case U_NAV:
+    case U_MOUSE:
+    case U_MEDIA:
+      planck_ez_left_led_on();
+      break;
+    case U_NUM:
+    case U_SYM:
+    case U_FUN:
+      planck_ez_right_led_on();
+      break;
+    case U_BUTTON:
+      planck_ez_left_led_on();
+      planck_ez_right_led_on();
+      break;
+    default:
+      break;
+    }
+    return state;
 }
 
 
