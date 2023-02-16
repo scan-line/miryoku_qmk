1a1954e88c63112eb53f08ea56f91779d807242a
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-16 09:32:42 +0000

    Update custom.c

--- users/manna-harbour_miryoku/custom.c
+++ users/manna-harbour_miryoku/custom.c
@@ -5,15 +5,6 @@
 #include "manna-harbour_miryoku.h"
 
 
-// Qmk definitions
-
-// Duplicated from rgb_matrix.c
-#if !defined(RGB_MATRIX_MAXIMUM_BRIGHTNESS) || RGB_MATRIX_MAXIMUM_BRIGHTNESS > UINT8_MAX
-#    undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS UINT8_MAX
-#endif
-
-
 // Sounds
 
 #ifdef AUDIO_ENABLE
