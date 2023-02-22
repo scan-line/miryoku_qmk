ece71a10d1fe2b5116e04e2d6a3105e427989c44
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-22 08:39:06 +0000

    Update custom.c

--- users/manna-harbour_miryoku/custom.c
+++ users/manna-harbour_miryoku/custom.c
@@ -443,10 +443,10 @@ bool process_audio_toggle(keyrecord_t *record) {
 
 bool process_user_key(keyrecord_t *record) {
   // Placeholder - make key visible
-  if (record->event.pressed) {
-    tap_code16(KC_QUESTION);
-    tap_code16(KC_U);
-  }
+  if (record->event.pressed)
+    register_code16(KC_QUESTION);
+  else
+    unregister_code16(KC_QUESTION);
   return false;
 }
 
