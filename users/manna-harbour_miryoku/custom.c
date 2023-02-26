b662468f2fdae2a2442cd273fe7ea32dfe93d761
scan-line <123326927+scan-line@users.noreply.github.com>
2023-02-26 16:38:53 +0000

    Update custom.c

--- users/manna-harbour_miryoku/custom.c
+++ users/manna-harbour_miryoku/custom.c
@@ -187,6 +187,9 @@ bool process_os_mode(os_mode_t mode, keyrecord_t *record) {
   "3"               \
   SS_UP(X_RALT)
 #define OS_LNX_GBP  \
+  "todo"
+  
+#define UNUSED      \
   SS_DOWN(X_LALT)   \
   SS_DOWN(X_LSFT)   \
   "u"               \
